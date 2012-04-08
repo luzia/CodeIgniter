15717a7e295bf7c0dd72c88dd0d88d44b63d0b88
luzia <luzia@bravomix.com>
2012-04-08 15:13:54 +0800

    edit guestbook.php

--- application/controllers/guestbook.php
+++ application/controllers/guestbook.php
@@ -5,18 +5,29 @@ class Guestbook extends CI_Controller{
   public function index(){
     echo "Hi~ Luzia!"."<br>";
 	echo "welcome guestbook";
+	
   }
   
-  public function add($title,$comment){
+  public function add($title = null,$comment = null){
     echo "add" . $title . $comment;
+	$data = array(
+		'title'=> isset($title) ? $title : "",
+		'comment'=> isset($comment) ? $comment : "");
+	$this->load->view('add_form',$data);
   }
   
-  public function delete($id){
+  public function delete($id = null){
     echo "delete" . $id;
   }
   
-  public function edit($id,$title,$comment){
+  public function edit($id = null,$title = null,$comment = null){
     echo "edit" . $id . $title . $comment;
+	$data = array(
+		'id'=>$id,
+		'title'=>$title,
+		'comment'=>$comment);
+	$this->load->view('add_form',$data);
+	
   }
 }
 
