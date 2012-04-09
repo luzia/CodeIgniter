6df54c87277b585f9a6c2fcaeb30238658ec4933
luzia <luzia@bravomix.com>
2012-04-09 16:55:42 +0800

    set database = test

--- application/controllers/guestbook.php
+++ application/controllers/guestbook.php
@@ -1,33 +1,73 @@
 <?php  
 class Guestbook extends CI_Controller{
   
-  
   public function index(){
-    echo "Hi~ Luzia!"."<br>";
-	echo "welcome guestbook";
-	
+	$this->load->view('/guestbook/view.html');
+    // echo "Hi~ Luzia!"."<br>";
+	// echo "welcome guestbook";
   }
   
-  public function add($title = null,$comment = null){
-    echo "add" . $title . $comment;
+  public function __construct(){
+		// Model «Øºc¨ç¦¡
+		parent::__construct();
+		$this->load->model('/guestbook_model');
+  }
+  
+  // public function add($title = null,$comment = null){
+    // echo "add" . $title . $comment;
+	// $data = array(
+		// 'title'=> isset($title) ? $title : "",
+		// 'comment'=> isset($comment) ? $comment : "");
+	// $this->load->view('add_form',$data);
+  // }
+  public function lists(){
+	$data['data'] = $this->guestbook_model->list_data();
+	$this->load->view('/guestbook/list',$data);
+  }
+  // public function view(){
+	// $this->load->view('guestbook/view.html');
+  // }
+  
+  public function add(){
+	$this->load->view('/guestbook/add_form');
+  }
+  
+  public function insert_db(){
 	$data = array(
-		'title'=> isset($title) ? $title : "",
-		'comment'=> isset($comment) ? $comment : "");
-	$this->load->view('add_form',$data);
+		'name'=>$this->input->get_post('title'),
+		'message'=>$this->input->get_post('message')
+		);
+	$this->guestbook_model->insert_data('message',$data);
+	// $data['data'] = $this->guestbook_model->list_data();
+	// $this->load->view('/guestbook/list',$data);
+	$this->load->view('/guestbook/view.html');
   }
   
-  public function delete($id = null){
-    echo "delete" . $id;
+  public function delete(){
+	$id = $this->input->get_post('id');
+	$this->guestbook_model->delete_db($id);
+	// $data['data'] = $this->guestbook_model->list_data();
+	// $this->load->view('/guestbook/list',$data);
+	$this->load->view('/guestbook/view.html');
   }
   
-  public function edit($id = null,$title = null,$comment = null){
-    echo "edit" . $id . $title . $comment;
-	$data = array(
-		'id'=>$id,
-		'title'=>$title,
-		'comment'=>$comment);
-	$this->load->view('add_form',$data);
-	
+  public function edit(){
+	$id = $this->input->get_post('id');
+	$data['data'] = $this->guestbook_model->get_data($id);
+	$this->load->view('/guestbook/edit_form',$data);
+	// $this->load->view('/guestbook/view.html');
+  }
+  
+  public function edit_db(){
+	$id = $this->input->get_post('id') ? $this->input->get_post('id') : "";
+	$update_data = array(
+		"name"=>$this->input->get_post('name') ? $this->input->get_post('name') : "",
+		"message"=>$this->input->get_post('message') ? $this->input->get_post('message') : ""
+	);
+	$this->guestbook_model->update_db($id,$update_data);
+	// $data['data'] = $this->guestbook_model->list_data();
+	// $this->load->view('/guestbook/list',$data);
+	$this->load->view('/guestbook/view.html');
   }
 }
 
